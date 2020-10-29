79f58fe8a4b1d146c80425bbbe8f71ef18ba5dc0
Gjfei <1491180173@qq.com>
2020-10-29 22:27:23 +0800

    feat: add script

--- src/main/index.js
+++ src/main/index.js
@@ -1,14 +1,10 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron'); //引入electron模块
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  win.loadFile('index.html')
-  win.webContents.openDevTools()
-}
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({ width: 400, height: 400 }); //设置打开的窗口大小
+  mainWindow.loadURL('http://localhost:3000'); //加载那个页面
+  //监听关闭事件，把主窗口设置为null
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
