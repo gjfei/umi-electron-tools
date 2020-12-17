17f07b93fb4b2d47c863dd022a6f4f688911a88d
fei <1491180173@qq.com>
2020-12-17 19:33:34 +0800

    feat: 添加ts

--- script/build.js
+++ script/build.js
@@ -1,14 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-const dotenv = require('dotenv');
-const concurrently = require('concurrently');
-const envConfig = dotenv.parse(
-  fs.readFileSync(path.resolve(__dirname, '../.env.production')),
-);
-concurrently([
-  { command: 'umi build', name: 'build', env: envConfig },
-  // {
-  //   command: `wait-on http://localhost:${envConfig.PORT} && electron .`,
-  //   name: 'electron',
-  // },
-]);
+const webpack = require('webpack');
+const config = require('../build/webpack.config.js');
+
+const compiler = webpack(config);
+
+compiler.run((err, stat) => {
+  console.log(stat);
+});
