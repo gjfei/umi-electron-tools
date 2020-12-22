c6d670ac7dc14ba957fe6482c3da6140779f0f8f
fei <1491180173@qq.com>
2020-12-22 15:02:35 +0800

    feat: 添加eslint

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,31 +1,31 @@
-import { app, BrowserWindow } from 'electron';
-import url from 'url';
-import path from 'path';
+import { app, BrowserWindow } from 'electron'
+import url from 'url'
+import path from 'path'
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development'
 
 app.on('ready', () => {
   let mainWindow: BrowserWindow | null = new BrowserWindow({
     width: 800,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
-    },
-  }); //设置打开的窗口大小
+      nodeIntegration: true
+    }
+  }) // 设置打开的窗口大小
 
   if (isDev) {
-    mainWindow.webContents.openDevTools();
-    mainWindow.loadURL('http://localhost:3000'); //加载那个页面
+    mainWindow.webContents.openDevTools()
+    mainWindow.loadURL('http://localhost:3000') // 加载那个页面
   } else {
     mainWindow.loadURL(
       url.format({
         pathname: path.join(__dirname, '../render/dist/index.html'),
         protocol: 'file:',
-        slashes: true,
+        slashes: true
       }),
-    );
+    )
   }
   mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-});
+    mainWindow = null
+  })
+})
